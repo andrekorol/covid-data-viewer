4e16fac7f66e77f06bcbe151076028678dfd8d4b
andrekorol <anrobits@yahoo.com.br>
2020-07-15 16:45:07 -0300

    Start writing the Jupyter Notebook

--- viewer.py
+++ viewer.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     from data import get_data
 
     csv_data = get_data()
-    df = pd.read_csv(csv_data)
+    df = pd.read_csv(csv_data)  # abre os dados em um pandas DataFrame
     print(df)
